046f2adc28d37e56d5e8f2bdb8068050abc3c746
campkhanin <campkhanin@gmail.com>
2025-11-12 16:45:52 +0700

    function for each page & minor fixes

--- back/model/Order.py
+++ back/model/Order.py
@@ -1,9 +1,9 @@
 import persistent
 
 class Order(persistent.Persistent):
-    def __init__(self, order_id, order_list, order_status, payment_status, total_price, date) -> None:
+    def __init__(self, order_id, menu_list, order_status, payment_status, total_price, date) -> None:
         self.order_id = order_id
-        self.order_list = order_list
+        self.menu_list = menu_list
         self.order_status = order_status
         self.payment_status = payment_status
         self.total_price = total_price
@@ -13,7 +13,7 @@ class Order(persistent.Persistent):
     def getOrderID(self):
         return self.order_id
     def getOrderList(self):
-        return self.order_list
+        return self.menu_list
     def getOrderStatus(self):
         return self.order_status
     def getPaymentStatus(self):
@@ -26,8 +26,8 @@ class Order(persistent.Persistent):
     # setter
     def setOrderID(self, order_id):
         self.order_id = order_id
-    def setOrderList(self, order_list):
-        self.order_list = order_list
+    def setOrderList(self, menu_list):
+        self.menu_list = menu_list
     def setOrderStatus(self, order_status):
         self.order_status = order_status
     def setPaymentStatus(self, payment_status):
--- back/model/User.py
+++ back/model/User.py
@@ -3,16 +3,41 @@ import persistent
 class User(persistent.Persistent):
     def __init__(
             self, name, email, phone, password, default_payment, 
-            order_id, location, order_list) -> None:
+            current, location, order_history, favourite, store,
+            coupon_list) -> None:
         self.name = name
         self.email = email 
         self.phone = phone
-        self.password = password
+        self.password = password # hashed password
         self.default_payment = default_payment
-        self.order_id = order_id
         self.location = location
-        self.order_list = order_list
+        self.order_history = order_history
+        self.current = current
+        self.favourite = favourite # list of favourite Store
+        self.store = store
+        self.coutpon_list = coupon_list
     
+    def add_favorite(self, store):
+        if store not in self.favourite:
+            self.favourite.append(store)
+    def add_order_history(self, order):
+        self.order_history.append(order)
+    def add_coupon(self, coupon):
+        self.coutpon_list.append(coupon)
+    
+    def remove_favorite(self, store):
+        if store in self.favourite:
+            self.favourite.remove(store)
+    def remove_coupon(self, coupon):
+        if coupon in self.coutpon_list:
+            self.coutpon_list.remove(coupon)
+            
+    def clear_favorites(self):
+        self.favourite = []
+    def clear_order_history(self):
+        self.order_history = []
+    def clear_coupons(self):
+        self.coutpon_list = []
     
 
     # getter
@@ -26,12 +51,18 @@ class User(persistent.Persistent):
         return self.password
     def getDefaultPayment(self):
         return self.default_payment
-    def getOrderID(self):
-        return self.order_id
+    def getCurrent(self):
+        return self.current
     def getLocation(self):
         return self.location
-    def getOrderList(self):
-        return self.order_list
+    def getOrderHistory(self):
+        return self.order_history
+    def getFavorites(self):
+        return self.favourite
+    def getStore(self):
+        return self.store
+    def getCouponList(self):
+        return self.coutpon_list
     
     # setter
     def setName(self, name):
@@ -44,11 +75,17 @@ class User(persistent.Persistent):
         self.password = password
     def setDefaultPayment(self, default_payment):
         self.default_payment = default_payment
-    def setOrderID(self, order_id):
-        self.order_id = order_id
+    def setCurrent(self, current):
+        self.current = current
     def setLocation(self, location):
         self.location = location
-    def setOrderList(self, order_list):
-        self.order_list = order_list
+    def setOrderHistory(self, order_history):
+        self.order_history = order_history
+    def setFavorites(self, favourite):
+        self.favourite = favourite
+    def setStore(self, store):
+        self.store = store
+    def setCouponList(self, coupon_list):
+        self.coutpon_list = coupon_list
 
     
